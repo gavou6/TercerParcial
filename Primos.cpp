82dc77798b3c63e188c13e3bf3cca826b3e025d6
gavou6 <gavou6@gmail.com>
2024-05-20 16:18:58 -0600

    Function esPrimo added

--- Primos.cpp
+++ Primos.cpp
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+int esPrimo(int num) {
+    int i;
+    if (num <= 1) {
+        return 0;
+    }
+    for (i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
 int menu(){
 	int opc;
 	while(1){
