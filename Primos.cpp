a2e64df76f94653aab1ac0588cb684710b4efacf
gavou6 <gavou6@gmail.com>
2024-05-20 16:18:14 -0600

    Function menu and main added

--- Primos.cpp
+++ Primos.cpp
@@ -1,1 +1,33 @@
+#include <stdio.h>
+
+int menu(){
+	int opc;
+	while(1){
+		system("cls");
+		printf("Bienvenido al menu, seleccione la opcion que desee:\n");
+		printf("1.-Ingresar un numero para saber si es primo o no\n");
+		printf("2.-Ingresar un rango y mostrar todos los nummeros primos dentro de este\n");
+		printf("3.-Salir\n");
+		scanf("%i", &opc);
+		switch(opc){
+			case 1:
+				saberPrimo();
+				system("pause");
+				break;
+			case 2:
+				rangoPrimo();
+				system("pause");
+				break;
+			case 3:
+				return 0;
+			default:
+                printf("Opcion no valida, intente de nuevo.\n");
+                system("pause");
+		}
+	}
+}
+
+int main(){
+	menu();
+	return 0;
 
